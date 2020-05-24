eb4414e153a0b9de4d45c9f5f7b1858581896589
m0nq <forgetmenaught@gmail.com>
2020-05-24 15:30:28 -0700

    Capture input events for checkboxes and generate password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw-generator';
+  length = 0;
+  password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
 
   onButtonClick() {
-    console.log('Button was clicked');
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '`~!@#$%^&*()_+-=[]\\{}|;\"\',./<>?';
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i += 1) {
+      generatedPassword += validChars[Math.floor(Math.random() * validChars.length)];
+    }
+    this.password = generatedPassword;
+  }
+
+  onLetterChange() {
+    this.includeLetters = !this.includeLetters;
+    console.log('letter checked ->', this.includeLetters);
+  }
+
+  onNumberChange() {
+    this.includeNumbers = !this.includeNumbers;
+    console.log('number checked ->', this.includeNumbers);
+  }
+
+  onSymbolChange() {
+    this.includeSymbols = !this.includeSymbols;
+    console.log('symbol checked ->', this.includeSymbols);
+  }
+
+  onLengthChange(value: string) {
+    const parseNumber = parseInt(value, 10);
+    if (!isNaN(parseNumber)) {
+      this.length = parseNumber;
+    }
   }
 }
